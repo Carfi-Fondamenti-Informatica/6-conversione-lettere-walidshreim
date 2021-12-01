1ab08c7f5a18c0124fbd458b5eca2d216fe812d1
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-12-01 23:30:40 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,17 +1,1 @@
-char letter(char c){
-    char b;
-
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
-        cout << "true"<< endl;
-        if(c >= 'a' && c <= 'z'){
-        b=c-32;
-        cout<< b <<endl;}
-        if (c >= 'A' && c <= 'Z'){
-            b=c+32;
-            cout<< b <<endl;
-        }
-    }
-    else
-        cout<<"false"<<endl;
-}
-
+char letter(char);
