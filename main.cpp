a60b4e2cba6cbd71daca2e54a65062787ab60267
walidshreim <91838654+walidshreim@users.noreply.github.com>
2021-12-01 22:36:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+using namespace std;
+char letter(char c){
+    char b;
 
-int main(){
-  return 0;
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+        cout << "true"<< endl;
+        if(c >= 'a' && c <= 'z'){
+        b=c-32;
+        cout<< b <<endl;}
+        if (c >= 'A' && c <= 'Z'){
+            b=c+32;
+            cout<< b <<endl;
+        }
+    }
+    else
+        cout<<"false"<<endl;
+
+}
+int main() {
+    char a;
+    cin >> a;
+    letter (a);
+
+
 }
